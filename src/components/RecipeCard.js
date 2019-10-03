93c8997d5b7e7e3985e1fcad8fb741ab6349c9ab
nirch <nirh2@yahoo.com>
2019-10-03 13:43:46 +0300

    recipe card desgin

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
 
 class RecipeCard extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        const {recipe} = this.props;
+    render() {
+        const { recipe } = this.props;
         return (
-            <p>{recipe.name}</p>
+            <Card className="recipe">
+                <Card.Img variant="top" src={recipe.img} />
+                <Card.Body>
+                    <Card.Title>{recipe.name}</Card.Title>
+                    <Card.Text>
+                        {recipe.desc}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         );
     }
 }
