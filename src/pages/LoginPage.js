ec69cced6d2938a78437326f0dbc6f039864eeb0
nirch <nirh2@yahoo.com>
2019-10-03 11:45:13 +0300

    login page design

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,16 +1,39 @@
 import React from 'react'
+import { Form, Button } from 'react-bootstrap'
 
 class LoginPage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
 
-    render() { 
-        return (  
-            <h1>Login</h1>
+    render() {
+        return (
+            <div className="login">
+                <h1>Login to Recipe Book</h1>
+                <p>or <a href="#/signup">create an account</a></p>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="success" type="submit" block>
+                        Login
+                    </Button>
+                </Form>
+
+            </div>
+
         );
     }
 }
- 
+
 export default LoginPage;
