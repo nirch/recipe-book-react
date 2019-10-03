008431b53f34841f8d369213bf7939dc4edfec0b
nirch <nirh2@yahoo.com>
2019-10-03 10:11:40 +0300

    adding bootstrap

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom'
+
 
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,25 @@
 import React from 'react'
+import {Jumbotron, Button} from 'react-bootstrap'
 
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
 
-    render() { 
-        return (  
-            <h1>Home</h1>
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <h1 className="display-3">Recipe Book</h1>
+                    <p>Master your recipes</p>
+                    <p>
+                        <Button variant="primary">Login</Button>
+                    </p>
+                </Jumbotron>
+            </div>
         );
     }
 }
- 
+
 export default HomePage;
