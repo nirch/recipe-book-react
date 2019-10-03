05bd523af1a8bffca0ca7c61ab980dc778398b58
nirch <nirh2@yahoo.com>
2019-10-03 09:58:52 +0300

    react router + routes and pages for home, login and recipes

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+
+    render() { 
+        return (  
+            <h1>Home</h1>
+        );
+    }
+}
+ 
+export default HomePage;
